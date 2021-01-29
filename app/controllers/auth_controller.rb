4c8e63cdd86366475c66a346e89d916fd331b87a
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2021-01-29 12:09:20 -0600

    finished

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,9 +5,8 @@ class AuthController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-    
-      render json: user
-     
+      
+      render json:  user
     else
       render json: { error: 'Username or Password does not exist.'}
     end
